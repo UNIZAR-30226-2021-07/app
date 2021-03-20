23c640a4e91492f0a79b626cec3b94dca5ee2352
Tomenos18 <tomas.pel.ben@gmail.com>
2021-03-20 18:32:22 +0100

    pantalla editar perfil, el boton de cambiar de tablero es provisional

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gatovidapp/perfil/profileConf.dart';
-import 'package:gatovidapp/perfil/profile.dart';
 
 
 void main()=> runApp(MaterialApp(
