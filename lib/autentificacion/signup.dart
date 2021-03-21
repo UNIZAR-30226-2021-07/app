2375f0cad16009ad51715422620b8de5e76f905c
Leticia-23 <leti2723@gmail.com>
2021-03-21 13:54:00 +0100

    Estructura ok, RenderFlex por arreglar

--- lib/autentificacion/signup.dart
+++ lib/autentificacion/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
     
 class Signup extends StatefulWidget {
   @override
@@ -18,14 +19,119 @@ class _SignupState extends State<Signup> {
       body: SafeArea(
         child: Column(
           children: <Widget>[
-            SizedBox(height: 20.0),
-            TextFormField(
-              validator: (value){
-                if (value.isEmpty) {
-                  return 'Introduzca nombre';
-                }
-            }
-            )
+            SizedBox(height: 40.0),
+            Row (
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+                  child: Text(
+                      'Nombre de usuario'
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Texto',
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 1.0
+                    )
+                  )
+                ),
+              ),
+            ),
+            SizedBox(height: 25.0),
+            Row (
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+                  child: Text(
+                      'Contraseña'
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    hintText: 'Texto',
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1.0
+                        )
+                    )
+                ),
+              ),
+            ),
+            SizedBox(height: 25.0),
+            Row (
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+                  child: Text(
+                      'Email'
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    hintText: 'Texto',
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1.0
+                        )
+                    )
+                ),
+              ),
+            ),
+            SizedBox(height: 40.0),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: 25.0, right: 25.0),
+              child: ElevatedButton(
+                onPressed: (){},
+                  style:
+                  ElevatedButton.styleFrom(
+                      primary: Colors.purple[800],
+                      onPrimary: Colors.white,
+                      minimumSize: Size(300.0, 50.0)
+                  ),
+                child: Text("REGISTRARSE"
+                )
+              )
+            ),
+            SizedBox(height: 30.0),
+            Row (mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+                  child: Text(
+                      '¿Ya tienes cuenta?'
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 50.0),
+                child: ElevatedButton(
+                    onPressed: (){},
+                    style:
+                    ElevatedButton.styleFrom(
+                        primary: Colors.purple[800],
+                        onPrimary: Colors.white,
+                        minimumSize: Size(150.0, 30.0)
+                    ),
+                    child: Text("Iniciar Sesión"
+                    )
+                )
+            ),
           ],
         ),
       ),
