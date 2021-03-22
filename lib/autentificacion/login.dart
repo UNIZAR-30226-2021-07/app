01374d91f54d981b4f619fae4a0459c213aab1f9
xzebra <zebrv.apps@gmail.com>
2021-03-22 13:27:55 +0100

    hide password

--- lib/autentificacion/login.dart
+++ lib/autentificacion/login.dart
@@ -12,7 +12,8 @@ class _LoginState extends State<Login> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.greenAccent[700],
-        title: Text('Inicio de sesión',
+        title: Text(
+          'Inicio de sesión',
           style: TextStyle(
             color: Colors.black,
             fontSize: 30.0,
@@ -26,16 +27,15 @@ class _LoginState extends State<Login> {
         child: Column(
           children: <Widget>[
             SizedBox(height: 70.0),
-            Row (
+            Row(
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
                   child: Text(
                     'Email',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0
-                    ),
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
                   ),
                 ),
               ],
@@ -46,27 +46,23 @@ class _LoginState extends State<Login> {
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(
-                          width: 1.0
-                      )
-                  ),
+                      borderSide: BorderSide(width: 1.0)),
                   hintText: 'Texto',
-                  isDense: true,                      // Added this
+                  isDense: true, // Added this
                   contentPadding: EdgeInsets.all(14),
                 ),
               ),
             ),
             SizedBox(height: 70.0),
-            Row (
+            Row(
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
                   child: Text(
                     'Contraseña',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0
-                    ),
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
                   ),
                 ),
               ],
@@ -74,50 +70,46 @@ class _LoginState extends State<Login> {
             Padding(
               padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
               child: TextFormField(
+                // Para ocultar la contraseña
+                autocorrect: false,
+                enableSuggestions: false,
+                obscureText: true,
+                // --
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(
-                          width: 1.0
-                      )
-                  ),
+                      borderSide: BorderSide(width: 1.0)),
                   hintText: 'Texto',
-                  isDense: true,                      // Added this
+                  isDense: true, // Added this
                   contentPadding: EdgeInsets.all(14),
                 ),
               ),
             ),
             SizedBox(height: 90.0),
             Padding(
-                padding: EdgeInsets.only(
-                    left: 25.0, right: 25.0),
+                padding: EdgeInsets.only(left: 25.0, right: 25.0),
                 child: ElevatedButton(
-                    onPressed: (){},
-                    style:
-                    ElevatedButton.styleFrom(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
                         primary: Colors.purple[800],
                         onPrimary: Colors.white,
-                        minimumSize: Size(300.0, 50.0)
-                    ),
-                    child: Text("INICIAR SESIÓN",
+                        minimumSize: Size(300.0, 50.0)),
+                    child: Text(
+                      "INICIAR SESIÓN",
                       style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0
-                      ),
-                    )
-                )
-            ),
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                    ))),
             SizedBox(height: 25.0),
-            Row (mainAxisAlignment: MainAxisAlignment.center,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
                   child: Text(
                     '¿Nuevo en Gatovid?',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0
-                    ),
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
                   ),
                 ),
               ],
@@ -125,26 +117,19 @@ class _LoginState extends State<Login> {
             Padding(
                 padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 50.0),
                 child: ElevatedButton(
-                    onPressed: (){},
-                    style:
-                    ElevatedButton.styleFrom(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
                         primary: Colors.purple[800],
                         onPrimary: Colors.white,
-                        minimumSize: Size(200.0, 40.0)
-                    ),
-                    child: Text("Crear una cuenta",
+                        minimumSize: Size(200.0, 40.0)),
+                    child: Text(
+                      "Crear una cuenta",
                       style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0
-                      ),
-                    )
-                )
-            ),
+                          fontWeight: FontWeight.bold, fontSize: 18.0),
+                    ))),
           ],
         ),
       ),
     );
   }
 }
-    
-
