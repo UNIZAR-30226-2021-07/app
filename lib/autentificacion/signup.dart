76c7e10df5b0b2ae34ac4a43ac350f73e2005087
xzebra <zebrv.apps@gmail.com>
2021-03-22 13:22:24 +0100

    hide password

--- lib/autentificacion/signup.dart
+++ lib/autentificacion/signup.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-    
+
 class Signup extends StatefulWidget {
   @override
   _SignupState createState() => _SignupState();
@@ -12,12 +12,13 @@ class _SignupState extends State<Signup> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.greenAccent[700],
-        title: Text('Crear una cuenta',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 30.0,
-          fontWeight: FontWeight.bold,
-        ),
+        title: Text(
+          'Crear una cuenta',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         centerTitle: true,
         automaticallyImplyLeading: false,
@@ -26,16 +27,15 @@ class _SignupState extends State<Signup> {
         child: Column(
           children: <Widget>[
             SizedBox(height: 40.0),
-            Row (
+            Row(
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
                   child: Text(
-                      'Nombre de usuario',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0
-                    ),
+                    'Nombre de usuario',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
                   ),
                 ),
               ],
@@ -46,27 +46,23 @@ class _SignupState extends State<Signup> {
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(
-                      width: 1.0
-                    )
-                  ),
+                      borderSide: BorderSide(width: 1.0)),
                   hintText: 'Texto',
-                  isDense: true,                      // Added this
+                  isDense: true, // Added this
                   contentPadding: EdgeInsets.all(14),
                 ),
               ),
             ),
             SizedBox(height: 40.0),
-            Row (
+            Row(
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
                   child: Text(
                     'Contraseña',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0
-                    ),
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
                   ),
                 ),
               ],
@@ -74,30 +70,31 @@ class _SignupState extends State<Signup> {
             Padding(
               padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
               child: TextFormField(
+                // Para ocultar la contraseña
+                autocorrect: false,
+                enableSuggestions: false,
+                obscureText: true,
+                // --
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(
-                          width: 1.0
-                      )
-                  ),
+                      borderSide: BorderSide(width: 1.0)),
                   hintText: 'Texto',
-                  isDense: true,                      // Added this
+                  isDense: true, // Added this
                   contentPadding: EdgeInsets.all(14),
                 ),
               ),
             ),
             SizedBox(height: 40.0),
-            Row (
+            Row(
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
                   child: Text(
                     'Email',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0
-                    ),
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
                   ),
                 ),
               ],
@@ -108,47 +105,38 @@ class _SignupState extends State<Signup> {
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(
-                          width: 1.0
-                      )
-                  ),
+                      borderSide: BorderSide(width: 1.0)),
                   hintText: 'Texto',
-                  isDense: true,                      // Added this
+                  isDense: true, // Added this
                   contentPadding: EdgeInsets.all(14),
                 ),
               ),
             ),
             SizedBox(height: 50.0),
             Padding(
-              padding: EdgeInsets.only(
-                  left: 25.0, right: 25.0),
-              child: ElevatedButton(
-                onPressed: (){},
-                  style:
-                  ElevatedButton.styleFrom(
-                      primary: Colors.purple[800],
-                      onPrimary: Colors.white,
-                      minimumSize: Size(300.0, 50.0)
-                  ),
-                child: Text("REGISTRARSE",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0
-                  ),
-                )
-              )
-            ),
+                padding: EdgeInsets.only(left: 25.0, right: 25.0),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.purple[800],
+                        onPrimary: Colors.white,
+                        minimumSize: Size(300.0, 50.0)),
+                    child: Text(
+                      "REGISTRARSE",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                    ))),
             SizedBox(height: 25.0),
-            Row (mainAxisAlignment: MainAxisAlignment.center,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 3.0, horizontal: 40.0),
                   child: Text(
-                      '¿Ya tienes cuenta?',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0
-                    ),
+                    '¿Ya tienes cuenta?',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
                   ),
                 ),
               ],
@@ -156,26 +144,19 @@ class _SignupState extends State<Signup> {
             Padding(
                 padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 50.0),
                 child: ElevatedButton(
-                    onPressed: (){},
-                    style:
-                    ElevatedButton.styleFrom(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
                         primary: Colors.purple[800],
                         onPrimary: Colors.white,
-                        minimumSize: Size(200.0, 40.0)
-                    ),
-                    child: Text("Iniciar Sesión",
+                        minimumSize: Size(200.0, 40.0)),
+                    child: Text(
+                      "Iniciar Sesión",
                       style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0
-                      ),
-                    )
-                )
-            ),
+                          fontWeight: FontWeight.bold, fontSize: 18.0),
+                    ))),
           ],
         ),
       ),
     );
   }
 }
-    
-
