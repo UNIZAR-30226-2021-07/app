82590b377e499dee247afc1a6f6fce2c7f5f2c70
xzebra <zebrv.apps@gmail.com>
2021-03-24 00:48:49 +0100

    basic test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,16 +5,17 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:gatovidapp/autentificacion/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gatovidapp/autentificacion/login.dart';
 
 void main() {
-  // testWidgets('build app', (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(Login());
+  testWidgets('build app', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(new MediaQuery(
+        data: new MediaQueryData(), child: new MaterialApp(home: new Login())));
 
-  //   // Verificar que hay botón de inicio de sesión
-  //   expect(find.text('INICIAR SESIÓN'), findsOneWidget);
-  // });
+    // Verificar que hay botón de inicio de sesión
+    expect(find.text('INICIAR SESIÓN'), findsOneWidget);
+  });
 }
