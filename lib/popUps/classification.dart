b4ae67366dd08eccb2c0c627ef4023c0ac3f581c
Tomenos18 <tomas.pel.ben@gmail.com>
2021-05-17 19:53:29 +0200

    una ñapa que me decia que no hasSize

--- lib/popUps/classification.dart
+++ lib/popUps/classification.dart
@@ -18,36 +18,39 @@ class Classification extends StatelessWidget {
         actions: <Widget>[
           classificationTable(context),
           Container(child: SizedBox(height: screenHeight * 0.05)),
-          Row(
-            children: [
-              Expanded(child: SizedBox()),
-              Expanded(
-                flex: 4,
-                child: ElevatedButton(
-                  child: const Text(
-                    "Cerrar",
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0xff6a1b9a),
-                    onPrimary: Colors.white,
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(30.0),
+          Container(
+            width: screenSize.width,
+            child: Row(
+              children: [
+                Expanded(child: SizedBox()),
+                Expanded(
+                  flex: 4,
+                  child: ElevatedButton(
+                    child: const Text(
+                      "Cerrar",
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold),
                     ),
-                    minimumSize: Size(MediaQuery.of(context).size.width * 0.2,
-                        MediaQuery.of(context).size.height * 0.075),
-                    shadowColor: Colors.black,
-                    side: BorderSide(color: Colors.white, width: 2),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff6a1b9a),
+                      onPrimary: Colors.white,
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0),
+                      ),
+                      minimumSize: Size(MediaQuery.of(context).size.width * 0.2,
+                          MediaQuery.of(context).size.height * 0.075),
+                      shadowColor: Colors.black,
+                      side: BorderSide(color: Colors.white, width: 2),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                          '/', (Route<dynamic> route) => false);
+                    },
                   ),
-                  onPressed: () {
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                        '/', (Route<dynamic> route) => false);
-                  },
                 ),
-              ),
-              Expanded(child: SizedBox())
-            ],
+                Expanded(child: SizedBox())
+              ],
+            ),
           )
         ],
       ),
